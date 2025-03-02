11fe51b89ff2b60f2c398c10e8a9bb955535e784
김원태 <rladnjsxo123@naver.com>
2025-03-02 19:20:56 +0900

    205. 동적 페이지 메타데이터 구성

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,5 +1,19 @@
-import Posts from '@/components/posts';
-import { getPosts } from '@/lib/posts';
+import Posts from "@/components/posts";
+import { getPosts } from "@/lib/posts";
+
+// export const metadata = {
+//   title: "All Post",
+//   description: "Brwose all our posts",
+// };
+
+export async function generateMetadata() {
+  const posts = await getPosts();
+
+  return {
+    title: `Browse out all ${posts.length} posts`,
+    description: "Brwose all our posts",
+  };
+}
 
 export default async function FeedPage() {
   const posts = await getPosts();
