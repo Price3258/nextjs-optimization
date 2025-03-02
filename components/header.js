07b9a62d497d1fec5f68b1bdf807332373ef287f
김원태 <rladnjsxo123@naver.com>
2025-03-02 18:56:39 +0900

    200 알 수 없는 이미지 업로드

--- components/header.js
+++ components/header.js
@@ -6,7 +6,11 @@ export default function Header() {
   return (
     <header id="main-header">
       <Link href="/">
-        <Image src={logo} alt="Mobile phone with posts feed on it" />
+        <Image
+          src={logo}
+          alt="Mobile phone with posts feed on it"
+          sizes="10vw"
+        />
       </Link>
       <nav>
         <ul>
